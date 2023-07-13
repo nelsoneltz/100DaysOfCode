7ad8d9bd125132c321886ebab28719aa302b802c
Nelson <nelsonfse@gmail.com>
2023-07-13 20:13:08 -0300

    day29

--- 29_password_manager/main.py
+++ 29_password_manager/main.py
@@ -1,10 +1,47 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-def func():
-    print('HOOOORRRAY')
+def generate_password():
+
+    letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0','1','2','3','4','5','6','7','8','9']
+    symbols = ['!','?','#','$','%','&','(',')','*','+','=','-']
+
+    number_letters = random.randint(8,10)
+    number_numbers = random.randint(2,4)
+    number_symbols = random.randint(2,4)
+
+    password_numbers = [random.choice(letters) for _ in range(number_letters)]
+    password_letters = [random.choice(numbers) for _ in range(number_numbers)]
+    password_symbols = [random.choice(symbols) for _ in range(number_symbols)]
+    password_list = password_numbers + password_letters + password_symbols
+    random.shuffle(password_list)
+
+    password_word =''.join(password_list)
+    password_input.delete(0,END)
+    password_input.insert(string=password_word,index=0)
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def adicionar():
-    print('added')
+    website = website_input.get()
+    username = username_input.get()
+    password = password_input.get()
+    data = website + ' | ' + username + ' | ' + password + '\n'
+    if website == '' or username == '' or password == '':
+        messagebox.showerror(title='Alert',message="Don't leave blank fields")
+    else:
+        confirmation = messagebox.askokcancel(title=website, message=f"These are the detais entered: \nEmail: {username}\nPassword: {password}\n Is it ok to save?")
+
+        if confirmation:
+            with open('data.txt', 'a') as file:
+                file.write(data)
+            website_input.delete(0,END)
+            password_input.delete(0,END)
+            pyperclip.copy(password)
+            
+    
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -22,9 +59,11 @@ canvas.grid(column=1,row= 0)
 
 website_input = Entry()
 website_input.grid(column=1,row=1,columnspan=2,sticky="EW")
+website_input.focus()
 
 username_input = Entry()
 username_input.grid(column=1,row=2,columnspan=2,sticky="EW")
+username_input.insert(0,'your_email@email.com')
 
 password_input = Entry()
 password_input.grid(column=1,row=3,sticky="EW")
@@ -40,7 +79,7 @@ password_label = Label(text='Password:')
 password_label.grid(column=0,row=3)
 
 # 2 BUTTONS
-generate_password_button = Button(text='Generate Password', command=func)
+generate_password_button = Button(text='Generate Password', command=generate_password)
 generate_password_button.grid(column=2,row=3,sticky="EW")
 
 add_button = Button(text='Add', command = adicionar,width=35)
