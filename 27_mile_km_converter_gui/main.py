3237e6111cbcd89bec71448b1a258fbfa1ed5917
Nelson Eltz <nelsonfse@gmail.com>
2023-07-14 07:38:32 -0300

    day27

--- 27_mile_km_converter_gui/main.py
+++ 27_mile_km_converter_gui/main.py
@@ -11,6 +11,16 @@ window.minsize(width=500,height = 300)
 my_label = tkinter.Label(text='writing some text',font=('Aria',24,'bold'))
 my_label.pack()
 
+# Button
+def button_click():
+    my_label.config(text=f'{input.get()}')
+
+button = tkinter.Button(text='Click HERE!',command=button_click)
+button.pack()
+
+# Entry
+input = tkinter.Entry(width=10)
+input.pack()
 
 
 
