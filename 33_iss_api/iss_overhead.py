d92871ec5f4bd679028e79b95d7495e7d7e8d238
Nelson <nelsonfse@gmail.com>
2023-07-17 18:53:53 -0300

    day33

---
+++ 33_iss_api/iss_overhead.py
@@ -0,0 +1,41 @@
+import requests
+from datetime import datetime
+
+
+MY_LAT = -20.275448
+MY_LONG = -40.290239
+
+def is_iss_overhead():
+
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    if MY_LAT-5 <= iss_latitude  <= MY_LAT +5 and MY_LONG -5 <= iss_longitude <= MY_LONG+5:
+        return True
+
+#Your position is within +5 or -5 degrees of the ISS position.
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+if is_iss_overhead() and is_night():
+    print("Overhead")
