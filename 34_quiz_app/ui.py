7de49b00f2a1325e0a21bb9b78fbf1bcde09aed2
Nelson <nelsonfse@gmail.com>
2023-07-20 22:07:44 -0300

    day34

--- 34_quiz_app/ui.py
+++ 34_quiz_app/ui.py
@@ -25,7 +25,7 @@ class QuizzInterface:
                          padx=20,
                          pady=20)
         
-        self.score_label = Label(text= 'Score: 1',
+        self.score_label = Label(text= 'Score: 0',
                                  bg=THEME_COLOR,
                                  fg='white')
         self.score_label.grid(column=1,
@@ -34,14 +34,16 @@ class QuizzInterface:
         true_button_image = PhotoImage(file='./images/true.png')
 
         self.true_button = Button(image=true_button_image,
-                              highlightthickness=0)
+                              highlightthickness=0,
+                              command=self.true_pressed)
         self.true_button.grid(column=0,
                               row=2)
 
         false_button_image = PhotoImage(file='./images/false.png')
 
         self.false_button = Button(image= false_button_image,
-                              highlightthickness=0)
+                              highlightthickness=0,
+                              command=self.false_pressed)
         self.false_button.grid(column=1,
                                row=2)
         
@@ -50,5 +52,26 @@ class QuizzInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text,text=q_text)
+        self.canvas.config(bg='white')
+        if self.quiz.still_has_questions():
+            
+            self.score_label.config(text=f'Score: {self.quiz.score}')
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text,text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text,text="You've reached the end of the quiz! Thank you for playing.")
+            self.true_button.config(state='disabled')
+            self.false_button.config(state='disabled')
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer('True'))
+
+    def false_pressed(self):
+        self.give_feedback(self.quiz.check_answer('False'))
+
+    def give_feedback(self,is_right):
+        if is_right:
+            self.canvas.config(bg='green')
+        else:
+            self.canvas.config(bg='red')
+        self.window.after(1000,self.get_next_question)
+
