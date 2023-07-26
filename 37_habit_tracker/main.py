e623a51dd56cca6b5c73df62505df754fdae53d0
Nelson <nelsonfse@gmail.com>
2023-07-25 22:09:24 -0300

    day37

---
+++ 37_habit_tracker/main.py
@@ -0,0 +1,16 @@
+import requests
+
+
+pixela_endpoint = 'https://pixe.la/v1/users'
+
+user_params = {
+    'token':"AZASXSDCD",
+    "username":'user1nf',
+    "agreeTermsofService":"yes",
+    "notMinor":"yes",
+}
+
+
+# response = requests.post(pixela_endpoint,json=user_params)
+# print(response.text)
+
