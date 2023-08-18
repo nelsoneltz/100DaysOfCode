b0b049923ff21f3777e33cbae086182783e68c74
Nelson <nelsonfse@gmail.com>
2023-08-17 23:10:57 -0300

    day10

--- 10_input_calculator/calculator.py
+++ 10_input_calculator/calculator.py
@@ -1,12 +1,15 @@
 print("CALCULATOR")
 
-
+# + FUNC
 def add(n1,n2):
     return n1+n2
+# - FUNC
 def subtract(n1,n2):
     return n1-n2
+# x FUNC
 def multiply(n1,n2):
     return n1*n2
+# / FUNC
 def divide(n1,n2):
     return n1/ n2
 
